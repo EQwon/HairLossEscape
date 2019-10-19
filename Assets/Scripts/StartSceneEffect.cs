76f8f07ea0e2bfc2ece31dccb3b3c95cbb398f5d
EQwon <bluetwintail0906@gmail.com>
2019-10-20 06:16:15 +0900

    시작화면 구성

---
+++ Assets/Scripts/StartSceneEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneEffect : MonoBehaviour
+{
+    [SerializeField] private GameObject effect;
+
+    public void CreateEffect()
+    {
+        Instantiate(effect, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
+    }
+}
